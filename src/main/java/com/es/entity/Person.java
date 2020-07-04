80470f1ca7a5c13b389761d2f5aa8a7ffa249c91
yc_glo <yc_glo@qq.com>
2020-07-04 13:05:11 +0800

    第二次

--- src/main/java/com/es/entity/Person.java
+++ src/main/java/com/es/entity/Person.java
@@ -1,16 +1,22 @@
 package com.es.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 import java.util.Set;
 
-@Document(indexName = "database01",type = "person")
+//@Document(indexName = "database01",type = "person")
+@ApiModel(value = "商品信息DTO")
 public class Person {
-    @Id
+//    @Id
+    @ApiModelProperty(name = "id", value = "序号", dataType = "string")
     private Integer id;
+    @ApiModelProperty(name = "personName", value = "序号", dataType = "string")
     private String personName;
-    private String age;
+    @ApiModelProperty(name = "age", value = "序号", dataType = "string")
+    private Integer age;
     private Set<String> interest;
 
     public Integer getId() {
@@ -29,13 +35,7 @@ public class Person {
         this.personName = personName;
     }
 
-    public String getAge() {
-        return age;
-    }
 
-    public void setAge(String age) {
-        this.age = age;
-    }
 
     public Set<String> getInterest() {
         return interest;
@@ -43,6 +43,15 @@ public class Person {
 
     public void setInterest(Set<String> interest) {
         this.interest = interest;
+    }
+
+    public Person(String personName, Integer age) {
+        this.personName = personName;
+        this.age = age;
+    }
+
+    public Integer getAge() {
+        return age;
     }
 
     @Override
